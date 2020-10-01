a1aba9d17bbb5dae3d92dd2d784b8870942542bc
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-10-02 00:06:12 +0530

    edited display messages

--- guess_the_number_game/script.py
+++ guess_the_number_game/script.py
@@ -2,31 +2,31 @@
 
 import random as rm
 
-num = rm.randint(0,100)
-#print(num)
+num = rm.randint(0, 100)
 chances = 4
 flag = 0
 
 n = int(input("Guess the randomly generated number = "))
-while chances>0:
-    if n==num:
-        print ("You have guessed the number.")
-        flag=1
+while chances > 0:
+    if n == num:
+        print("You have guessed the number.")
+        flag = 1
         break
-    elif n>100 or n<0:
-        print ("Invalid")
-    elif n-num>10:
-        print ("Too high")
-    elif num-n>10:
-        print ("Too low")
-    elif abs(n-num)>=5:
-        print ("Nearby")
-    elif abs(n-num)<5:
-        print("You're very close!")
-    chances-=1
+    elif n > 100 or n < 0:
+        print("Invalid")
+    elif n-num > 10:
+        print("The number you guessed is too high. Make a lower guess")
+    elif num-n > 10:
+        print("The number you guessed is too low. Make a higher guess")
+    elif abs(n-num) >= 5:
+        print("You're close!")
+    elif abs(n-num) < 5:
+        print("You're almost there!")
+    chances -= 1
     n = int(input("Guess again = "))
 
-if flag==1:
-    print("Congratulations, you won! You guessed it with",chances,"chances left.")
+if flag == 1:
+    print("Congratulations, you won! You guessed it with",
+          chances, "chances left.")
 else:
     print("You lost.")
