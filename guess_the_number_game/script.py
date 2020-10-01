27e0f286238d32ad046229d340cfd04621456a53
cyberdev25 <68549138+cyberdev25@users.noreply.github.com>
2020-10-01 20:54:00 +0530

    Update script.py

--- guess_the_number_game/script.py
+++ guess_the_number_game/script.py
@@ -6,27 +6,33 @@ num = rm.randint(0,100)
 #print(num)
 chances = 4
 flag = 0
-
-n = int(input("Guess the randomly generated number = "))
-while chances>0:
-    if n==num:
-        print ("You have guessed the number.")
-        flag=1
-        break
-    elif n>100 or n<0:
-        print ("Invalid")
-    elif n-num>10:
-        print ("Too high")
-    elif num-n>10:
-        print ("Too low")
-    elif abs(n-num)>=5:
-        print ("Nearby")
-    elif abs(n-num)<5:
-        print("You're very close!")
-    chances-=1
-    n = int(input("Guess again = "))
-
-if flag==1:
-    print("Congratulations, you won! You guessed it with",chances,"chances left.")
-else:
-    print("You lost.")
+#Adding exceptional handling to prevent the program from terminating if input value is non numeric
+try:
+    while chances>0:
+        try:
+            n = int(input('Enter your guess'))
+        except:
+            print("Sorry,this is not a numerical value , try again :( ")
+            continue
+        if n==num:
+            print ("You have guessed the number.")
+            flag=1
+            break
+        elif n>100 or n<0:
+            print ("Invalid")
+        elif n-num>10:
+            print ("Too high")
+        elif num-n>10:
+            print ("Too low")
+        elif abs(n-num)>=5:
+            print ("Nearby")
+        elif abs(n-num)<5:
+            print("You're very close!")
+            chances-=1
+            n = int(input("Guess again = "))
+            
+except KeyWordInterrupt:
+    if flag==1:
+        print("Congratulations, you won! You guessed it with",chances,"chances left.")
+    else:
+        print("You lost.")
