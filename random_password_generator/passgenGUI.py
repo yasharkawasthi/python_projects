c8aed13cf2b6b89c4fe3296aaf09910af5b34880
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2021-10-10 11:20:31 +0530

    Button labels

--- random_password_generator/passgenGUI.py
+++ random_password_generator/passgenGUI.py
@@ -12,26 +12,31 @@ frame = Frame(root)
 lengthSelect = IntVar()
 choice = IntVar()
 
+
 def strong():
     entryPass.delete(0, END)
     length = lengthSelect.get()
     password = string.ascii_letters + string.digits + "!@#$&*"
     return ''.join((random.choice(password) for i in range(length)))
 
+
 def generatePass():
     generatedPass = strong()
     entryPass.insert(10, generatedPass)
+
 
 def copypass():
     temp = entryPass.get()
     pyperclip.copy(temp)
 
-labelwelcome = Label(frame, text="Random Password Generator", font=('Arial Bold', 12))
+
+labelwelcome = Label(
+    frame, text="Random Password Generator", font=('Arial Bold', 12))
 labelPass = Label(frame, text="Password")
 labelLen = Label(frame, text="Length")
 entryPass = Entry(frame)
-generateBtn = Button(frame, text="Generate", command=generatePass)
-copyBtn = Button(frame, text="Copy", command=copypass)
+generateBtn = Button(frame, text="Generate Password", command=generatePass)
+copyBtn = Button(frame, text="Copy Password", command=copypass)
 labelmsg = Label(root, text="Developed by M. Shahnawaz Alam", relief=SUNKEN)
 combo = Combobox(frame, textvariable=lengthSelect)
 combo['values'] = (8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
